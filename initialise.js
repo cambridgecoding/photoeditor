b2e4b2033504071d1205b7f07d0c90aab015c52c
Raphaël Proust <raphlalou@gmail.com>
2016-06-22 14:35:28 +0100

    linter options for ES6

---
+++ initialise.js
@@ -0,0 +1,9 @@
+var exec = require('child_process').exec;
+function printer(error, stdout, stderr) {
+  if(error){
+    console.error('exec error: ' + error);
+  }
+  console.log(stdout);
+  console.warn(stderr);
+}
+exec("apm install linter-jshint", printer);
